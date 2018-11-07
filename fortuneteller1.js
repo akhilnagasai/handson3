06e852219f14d0cce816a62041520c63b16f4ab7
akhilnagasai <44810352+akhilnagasai@users.noreply.github.com>
2018-11-07 19:43:43 +0530

    Delete fortuneteller1.js

--- fortuneteller1.js
+++
@@ -1,9 +0,0 @@
-function tellFortune(jobTitle, geoLocation, partner, numKids) {
-    var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ' and married to ' +
-   partner + ' ' + ' with ' + numKids + ' kids.';
-    console.log(future);
-}
-
-tellFortune('football player', 'spain', 'Scooby doo', 3);
-tellFortune('captain of titanic', 'Japan', 'a house wife', 3000);
-tellFortune('king of the jungle', 'america', 'queen of the jungle', 0);
