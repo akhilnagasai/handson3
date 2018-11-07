c281508417f0970d1cbf72193380ef2c245a060f
akhilnagasai <gokukenshin35@gmail.com>
2018-11-07 17:12:26 +0530

    added 1st handson files

---
+++ evenodd.js
@@ -0,0 +1,13 @@
+var a,b
+console.log('odd numbers')
+for(i=0;i<20;i++)
+{
+ a=2*i+1
+ console.log(a)
+}
+console.log('even numbers')
+for(j=0;j<20;j++)
+{
+  b=2*j  
+  console.log(b)
+}    
---
+++ exp.js
@@ -0,0 +1,6 @@
+var a,b,c
+a+b==a+c
+if (b==c)
+{console.log("true")}
+else
+{console.log("false")}
