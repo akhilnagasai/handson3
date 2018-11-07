abc6f993e228a647acb5e7bb0d051b85a54c8d30
akhilnagasai <44810352+akhilnagasai@users.noreply.github.com>
2018-11-07 19:44:40 +0530

    Delete greatnum.js

--- greatnum.js
+++
@@ -1,8 +0,0 @@
-function greatnum(){
-  var x=1>2?1:2
-  var y=3>4?3:4
-  console.log(x +'is the greatest number')
-  console.log(y +'is the greatest number')
-}
-return greatnum()
-//console.log('-------')
