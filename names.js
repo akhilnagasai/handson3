751b027e13710b2217e4e367962c39d626bf2e21
akhilnagasai <44810352+akhilnagasai@users.noreply.github.com>
2018-11-07 19:45:56 +0530

    Delete names.js

--- names.js
+++
@@ -1,6 +0,0 @@
-var name = "akhil"
-var age = 19
-var db = 05+'/'+12+'/'+1998
-var address = 'shanthi nagar 16th road'
-var contactno = 7013352643
-console.log(name+'\n'+age+'\n'+db+'\n'+address+'\n'+contactno)
